51f1bcb8471932d2b8badc05ea3202afb7218a25
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-09-30 16:05:20 -0300

    add CustomerRepository interface

---
+++ src/main/core/contexts/customers/CustomerRepository.java
@@ -0,0 +1,21 @@
+package main.core.contexts.customers;
+
+import java.util.Optional;
+
+import main.core.models.users.Customer;
+import main.core.models.users.exceptions.EmailAlreadyExists;
+import main.core.models.users.types.CustomerID;
+import main.core.models.users.types.Email;
+import main.core.models.users.types.UserID;
+import main.core.models.users.types.Username;
+
+public interface CustomerRepository 
+{
+    void register(Customer customer) throws EmailAlreadyExists;
+    boolean exists(Email email);
+    boolean exists(Username username);
+    boolean exists(UserID id);
+    Optional<Customer> by(Email email);
+    Optional<Customer> by(Username username);
+    Optional<Customer> by(CustomerID id);
+}
