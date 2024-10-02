fe195c5f3c28ee009fdc931c29ddc342434c9f6f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-01 21:22:26 -0300

    [core.context.users] implement account availability

---
+++ src/main/core/contexts/users/subcontexts/AccountAvailability.java
@@ -0,0 +1,51 @@
+package main.core.contexts.users.subcontexts;
+
+import java.util.Optional;
+
+import main.core.contexts.Context;
+import main.core.models.users.types.Account;
+import main.core.roles.UserRepository;
+
+public class AccountAvailability
+extends Context<Account> 
+{
+    private UserRepository repository;
+    private Account account;
+    private boolean unavailableEmail;
+    private boolean unavailableUsername;
+
+    public AccountAvailability(UserRepository repository, Account account)
+    {
+        this.repository = repository;
+        this.account = account;
+    }
+
+    private void updateAvailability()
+    {
+        unavailableEmail = repository.exists(account.email());
+        unavailableUsername = repository.exists(account.username());
+    }
+
+    public boolean isAvailable()
+    {
+        updateAvailability();
+        return !(unavailableEmail || unavailableUsername);
+    }
+
+    public Optional<String> reason()
+    {
+        updateAvailability();
+
+        String reason;
+        if (unavailableEmail && unavailableUsername)
+            reason = "Email and Username are";
+        else if (unavailableEmail)
+            reason = "Email is";
+        else if (unavailableUsername)
+            reason = "Username is";
+        else
+            return Optional.empty();
+
+        return Optional.of(reason + " already registered");
+    }
+}
---
+++ src/test/contexts/customer/AccountAvailabilityFeature.java
@@ -0,0 +1,133 @@
+package test.contexts.customer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import main.core.contexts.users.subcontexts.AccountAvailability;
+import main.core.models.users.Customer;
+import main.core.models.users.types.Account;
+import main.core.models.users.types.CustomerID;
+import main.core.models.users.types.Email;
+import main.core.models.users.types.Username;
+import main.core.roles.UserRepository;
+import main.infra.virtualdb.VirtualUserRepository;
+
+public class AccountAvailabilityFeature 
+{
+    private Account account;
+    private UserRepository repository;
+
+    @BeforeEach
+    public void initAccount()
+    {
+        this.account = new Account(
+            new Username("john.doe"),
+            new Email("john@example.com"),
+            "12345678"
+        );
+        
+        this.repository = new VirtualUserRepository();
+        this.repository.register(new Customer(new CustomerID(), account));
+    }
+
+    /** Scenario: Checking an Account availability 
+     *    Given some Account
+     *      And a Repository without this Account
+     *    When checking availability
+     *      Then Account should be available
+     */
+    @Test
+    @DisplayName("Given some Account And a Repository without this Account")
+    public void shouldBeAvailable()
+    {
+        // When checking availability
+        var context = new AccountAvailability(
+            new VirtualUserRepository(),
+            account
+        );
+
+        assertTrue(context.isAvailable());
+        assertEquals(Optional.empty(), context.reason());
+    }
+
+    /** Scenario: Checking an Account availability 
+     *    Given some Account
+     *      And a Repository with the same Email
+     *    When checking availability
+     *      Then Account should be available
+     */
+    @Test
+    public void shouldBeUnavailable()
+    {
+        // When registering a new Account
+        var context = new AccountAvailability(
+            repository,
+            account
+        );
+
+        assertFalse(context.isAvailable());
+        assertEquals(
+            "Email and Username are already registered", 
+            context.reason().get()
+        );
+    }
+
+    /** Scenario: Checking an Account availability 
+     *    Given some Account
+     *      And a Repository with the same Email
+     *    When checking availability
+     *      Then Account should be available
+     */
+    @Test
+    public void shouldBeUnavailableForSameEmail()
+    {
+        // When registering a new Account
+        var context = new AccountAvailability(
+            repository,
+            new Account(
+                new Username(""), 
+                new Email("john@example.com"), 
+                ""
+            )
+        );
+
+        assertFalse(context.isAvailable());
+        assertEquals(
+            "Email is already registered", 
+            context.reason().get()
+        );
+    }
+
+    /** Scenario: Checking an Account availability 
+     *    Given some Account
+     *      And a Repository with the same Email
+     *    When checking availability
+     *      Then Account should be available
+     */
+    @Test
+    public void shouldBeUnavailableForSameUsername()
+    {
+        var context = new AccountAvailability(
+            repository,
+            new Account(
+                new Username("john.doe"), 
+                new Email(""), 
+                ""
+            )
+        );
+
+        assertFalse(context.isAvailable());
+        assertEquals(
+            "Username is already registered", 
+            context.reason().get()
+        );
+    }
+
+}
