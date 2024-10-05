e2e15d79fb795e1d74e6b591516a6ffd48f06225
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-05 14:13:39 -0300

    [test.models.events] implement Poster

---
+++ src/main/domain/models/events/Poster.java
@@ -0,0 +1,27 @@
+package main.domain.models.events;
+
+import java.time.LocalDate;
+
+public class Poster {
+    private String title;
+    private String description;
+    private LocalDate schedule;
+
+    /**
+     * @param title
+     * @param description
+     * @param schedule
+     */
+    public Poster(String title, String description, LocalDate schedule) {
+        this.title = title;
+        this.description = description;
+        this.schedule = schedule;
+    }
+
+    public String title() { return title; }
+
+    public String description() { return description; }
+
+    public LocalDate scheduledDate() { return schedule; }
+
+}
