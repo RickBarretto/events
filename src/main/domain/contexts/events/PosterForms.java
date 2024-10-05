6f6f53681cd001583f13c62736a3894ddd7398f9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-05 14:22:41 -0300

    [main.contexts.events] implement Poster's Forms

---
+++ src/main/domain/contexts/events/PosterForms.java
@@ -0,0 +1,36 @@
+package main.domain.contexts.events;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import main.domain.models.events.Poster;
+
+public class PosterForms {
+    private String title;
+    private String description;
+    private LocalDate schedule;
+
+    public PosterForms title(String title) {
+        this.title = title;
+        return this;
+    }
+    
+    public PosterForms description(String description) {
+        this.description = description;
+        return this;
+    }
+    
+    public PosterForms scheduledFor(LocalDate schedule) {
+        this.schedule = schedule;
+        return this;
+    }
+    
+    public Poster submit() throws NullPointerException {
+        Objects.requireNonNull(title);
+        Objects.requireNonNull(description);
+        Objects.requireNonNull(schedule);
+
+        return new Poster(title, description, schedule);
+    }
+
+}
