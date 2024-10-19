bff574aafdadc50b73ac037ba73e17e28c57c6d5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 18:07:32 -0300

    make it clearer with BDD

--- src/test/context/user/UserLoginFeature.java
+++ src/test/context/user/UserLoginFeature.java
@@ -1,138 +1,98 @@
 package test.context.user;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeFalse;
-import static org.junit.Assume.assumeTrue;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-
-import org.junit.jupiter.api.Nested;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 import org.junit.jupiter.api.Test;
 
 import main.domain.contexts.user.UserLogin;
 import main.domain.exceptions.PermissionDenied;
 import main.infra.Session;
 import main.roles.repositories.Users;
-import test.resources.bdd.Assume;
-import test.resources.bdd.Then;
-import test.resources.bdd.When;
+
+import test.resources.bdd.*;
 import test.resources.entities.ConcreteUsers;
 
+@Feature("User Login")
 public class UserLoginFeature {
+
     private final Users users = ConcreteUsers.withJohnAndJane();
 
+    @Scenario("Logging in as an existing user")
+    @Given("An anonymous user")
     @Assume("Session is inactive")
-    void shouldBeInactive(Session session) { assumeFalse(session.isActive()); }
-
-    @Assume("Session is active")
-    void shouldBeActive(Session session) { assumeTrue(session.isActive()); }
-
-    @Nested
-    class LogIn {
-        Session session;
-
-        @Test
-        @Assume("An anonymous user")
-        @When("Login as some existent user")
-        @Then("Should log in as this user")
-        void shouldLogInAsUser() {
-            // Given
-            var session = new Session();
-            shouldBeInactive(session);
-
-            // When
-            assertDoesNotThrow(() -> new UserLogin(users).withSession(session)
-                    .logAs("john.doe@example.com", "123456"));
-
-            // Then
-            assertEquals(ConcreteUsers.JohnDoe(), session.loggedUser().get());
-            assertTrue(session.isActive());
-        }
-
-        @Test
-        @Assume("A logged-in session")
-        @When("Log-in as other existent user")
-        @Then("Should change the current user to the new one")
-        void shouldLogInAsAnotherUser() {
-            // Given
-            var session = Session.loggedAs(ConcreteUsers.JohnDoe());
-            shouldBeActive(session);
-            assumeTrue(
-                    session.loggedUser().get().equals(ConcreteUsers.JohnDoe()));
-
-            // When
-            assertDoesNotThrow(() -> new UserLogin(users).withSession(session)
-                    .logAs("jane.doe@example.com", "789123"));
-
-            // Then
-            assertEquals(ConcreteUsers.JaneDoe(), session.loggedUser().get());
-            assertTrue(session.isActive());
-        }
-
-        @Test
-        @When("Try to log-in as an inexistent User")
-        @Then("Should throw, and keep the same Session state")
-        void shouldThrow() {
-            // Given
-            var session = Session.loggedAs(ConcreteUsers.JohnDoe());
-            shouldBeActive(session);
-            assumeTrue(
-                    session.loggedUser().get().equals(ConcreteUsers.JohnDoe()));
-
-            // When
-            assertThrowsExactly(PermissionDenied.class,
-                    () -> new UserLogin(users).withSession(session)
-                            .logAs("johane.doe@example.com", "789123"));
-            assertThrowsExactly(PermissionDenied.class,
-                    () -> new UserLogin(users).withSession(session)
-                            .logAs("jane.doe@example.com", "123456"));
-
-            // Then
-            assertEquals(ConcreteUsers.JohnDoe(), session.loggedUser().get());
-            assertTrue(session.isActive());
-        }
+    @When("Logging in as an existing user")
+    @Then("Should log in as this user")
+    @Test
+    void shouldLogInAsUser() {
+        var session = new Session();
+        assumeFalse(session.isActive());
+        assertDoesNotThrow(() -> new UserLogin(users).withSession(session)
+                .logAs("john.doe@example.com", "123456"));
+        assertEquals(ConcreteUsers.JohnDoe(), session.loggedUser().get());
+        assertTrue(session.isActive());
     }
 
-    @Nested
-    class LogOut {
-        Session session;
+    @Scenario("Changing login to another existing user")
+    @Given("A logged-in session")
+    @Assume("Session is active")
+    @When("Logging in as another existing user")
+    @Then("Should change the current user to the new one")
+    @Test
+    void shouldLogInAsAnotherUser() {
+        var session = Session.loggedAs(ConcreteUsers.JohnDoe());
+        assumeTrue(session.isActive());
+        assumeTrue(session.loggedUser().get().equals(ConcreteUsers.JohnDoe()));
+        assertDoesNotThrow(() -> new UserLogin(users).withSession(session)
+                .logAs("jane.doe@example.com", "789123"));
+        assertEquals(ConcreteUsers.JaneDoe(), session.loggedUser().get());
+        assertTrue(session.isActive());
+    }
 
-        @Test
-        @Assume("An anonymous user")
-        @When("Try to logout")
-        @Then("Should remain anonymous")
-        void shouldLogInAsUser() {
-            // Given
-            var session = new Session();
-            shouldBeInactive(session);
+    @Scenario("Trying to log in as a nonexistent user")
+    @Given("A logged-in session")
+    @Assume("Session is active")
+    @When("Trying to log in as a nonexistent user")
+    @Then("Should throw, and keep the same session state")
+    @Test
+    void shouldThrow() {
+        var session = Session.loggedAs(ConcreteUsers.JohnDoe());
+        assumeTrue(session.isActive());
+        assumeTrue(session.loggedUser().get().equals(ConcreteUsers.JohnDoe()));
+        assertThrowsExactly(PermissionDenied.class,
+                () -> new UserLogin(users).withSession(session)
+                        .logAs("johane.doe@example.com", "789123"));
+        assertThrowsExactly(PermissionDenied.class, () -> new UserLogin(users)
+                .withSession(session).logAs("jane.doe@example.com", "123456"));
+        assertEquals(ConcreteUsers.JohnDoe(), session.loggedUser().get());
+        assertTrue(session.isActive());
+    }
 
-            // When
-            new UserLogin(users).withSession(session).logOut();
+    @Scenario("Logging out as an anonymous user")
+    @Given("An anonymous user")
+    @Assume("Session is inactive")
+    @When("Trying to logout")
+    @Then("Should remain anonymous")
+    @Test
+    void shouldRemainAnonymousWhenLoggingOut() {
+        var session = new Session();
+        assumeFalse(session.isActive());
+        new UserLogin(users).withSession(session).logOut();
+        assertTrue(session.loggedUser().isEmpty());
+        assertFalse(session.isActive());
+    }
 
-            // Then
-            assertTrue(session.loggedUser().isEmpty());
-            assertFalse(session.isActive());
-        }
-
-        @Test
-        @Assume("A logged-in session")
-        @When("Loggin out")
-        @Then("Should disable the session")
-        void shouldLogInAsAnotherUser() {
-            // Given
-            var session = Session.loggedAs(ConcreteUsers.JohnDoe());
-            shouldBeActive(session);
-            assumeTrue(
-                    session.loggedUser().get().equals(ConcreteUsers.JohnDoe()));
-
-            // When
-            new UserLogin(users).withSession(session).logOut();
-
-            // Then
-            assertTrue(session.loggedUser().isEmpty());
-            assertFalse(session.isActive());
-        }
+    @Scenario("Logging out as a logged-in user")
+    @Given("A logged-in session")
+    @Assume("Session is active")
+    @When("Logging out")
+    @Then("Should disable the session")
+    @Test
+    void shouldDisableSessionWhenLoggingOut() {
+        var session = Session.loggedAs(ConcreteUsers.JohnDoe());
+        assumeTrue(session.isActive());
+        assumeTrue(session.loggedUser().get().equals(ConcreteUsers.JohnDoe()));
+        new UserLogin(users).withSession(session).logOut();
+        assertTrue(session.loggedUser().isEmpty());
+        assertFalse(session.isActive());
     }
 }
