8378b6b2a2054744ff4a13cd4edd77b8d2c8c346
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 14:52:18 -0300

    deprecate unused constructor

--- src/main/domain/models/events/EventId.java
+++ src/main/domain/models/events/EventId.java
@@ -4,8 +4,4 @@ import java.util.UUID;
 
 import main.roles.EntityId;
 
-public class EventId extends EntityId {
-    public EventId(UUID id) { super(id); }
-
-    public EventId() { super(); }
-}
+public class EventId extends EntityId { public EventId() { super(); } }
