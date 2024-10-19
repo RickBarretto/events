9f1e3b8eb19da3f9557329a7974f5ad2c63f18d6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 18:56:04 -0300

    make it clearer with BDD

--- src/test/context/event/EventRegisteringFeature.java
+++ src/test/context/event/EventRegisteringFeature.java
@@ -1,14 +1,16 @@
 package test.context.event;
 
-import static org.junit.Assert.*;
-import static org.junit.Assume.assumeFalse;
-import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.function.Supplier;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import main.domain.contexts.events.EventRegistering;
 import main.domain.contexts.events.forms.EventInformation;
@@ -20,9 +22,16 @@ import main.domain.models.events.Poster;
 import main.domain.models.users.User;
 import main.infra.virtual.EventsInMemory;
 import main.roles.repositories.Events;
-import test.resources.bdd.*;
+import test.resources.bdd.And;
+import test.resources.bdd.Assume;
+import test.resources.bdd.Feature;
+import test.resources.bdd.Given;
+import test.resources.bdd.Scenario;
+import test.resources.bdd.Then;
+import test.resources.bdd.When;
 import test.resources.entities.ConcreteUsers;
 
+@Feature("Registering an Event")
 public class EventRegisteringFeature {
 
     private Events repository;
@@ -44,86 +53,76 @@ public class EventRegisteringFeature {
         return repository.event("From Zero", LocalDate.of(2024, 10, 15));
     }
 
-    @Nested
     @Scenario("Successfully registering an Event")
-    @Given("Some Post, an Admin user and the Current Day")
-    class Successful {
-
-        @When("Registering a new future Event as an Admin")
-        void register() throws PermissionDenied, EventAlreadyRegistered,
-                CantRegisterPastEvent {
+    @Given("Some Post, an Admin user, and the Current Day")
+    @When("Registering a new future Event as an Admin")
+    @Then("Should register if User is Admin")
+    @And("Is a future Event")
+    @And("Event's title and date is Available")
+    @Test
+    void shouldRegister() {
+        Supplier<Boolean> isRegistered = () -> repository.has("From Zero",
+                LocalDate.of(2024, 10, 15));
+        // Precondition
+        assumeFalse(isRegistered.get());
+        // Do
+        assertDoesNotThrow(() -> {
             var poster = somePoster();
             var user = someAdmin();
             var currentDay = LocalDate.of(2024, 10, 1);
             new EventRegistering().into(repository).poster(poster).by(user)
                     .on(currentDay).register();
-        }
-
-        @Test
-        @Then("Should register if User is Admin,")
-        @And("And is a future Event")
-        @And("And Event's title and date is Available")
-        void shouldRegister() {
-            Supplier<Boolean> isRegistered = () -> repository.has("From Zero",
-                    LocalDate.of(2024, 10, 15));
-
-            // Precondition
-            assumeFalse("Event must not be registered", isRegistered.get());
-
-            // Do
-            assertDoesNotThrow(() -> this.register());
-
-            // Assertions
-            var event = actualEvent(repository);
-            assertTrue("Title and Date are now registered", isRegistered.get());
-            assertTrue("Event is present", event.isPresent());
-        }
-
-        @Test
-        @And("Registered event is the same")
-        void shouldBeTheSame() {
-            // Do
-            assertDoesNotThrow(() -> this.register());
-
-            // Assertions
-            var event = actualEvent(repository).get();
-            assertEquals("From Zero", event.poster().title());
-            assertEquals("Linkin Park's show", event.poster().description());
-            assertEquals(LocalDate.of(2024, 10, 15), event.poster().date());
-        }
+        });
+        // Assertions
+        var event = actualEvent(repository);
+        assertTrue(isRegistered.get());
+        assertTrue(event.isPresent());
     }
 
-    @Nested
-    @Scenario("Customer trying to register an Event")
-    @Given("Some Post, a Customer User and the Current Day")
-    class UnsuccessfulForCustomer {
+    @Scenario("Successfully registering an Event")
+    @Given("Some Post, an Admin user, and the Current Day")
+    @When("Registering a new future Event as an Admin")
+    @Then("Registered event is the same")
+    @Test
+    void shouldBeTheSame() {
+        // Do
+        assertDoesNotThrow(() -> {
+            var poster = somePoster();
+            var user = someAdmin();
+            var currentDay = LocalDate.of(2024, 10, 1);
+            new EventRegistering().into(repository).poster(poster).by(user)
+                    .on(currentDay).register();
+        });
+        // Assertions
+        var event = actualEvent(repository).get();
+        assertEquals("From Zero", event.poster().title());
+        assertEquals("Linkin Park's show", event.poster().description());
+        assertEquals(LocalDate.of(2024, 10, 15), event.poster().date());
+    }
 
-        @When("Trying to register a new future Event as a Customer")
-        void tryRegister() throws PermissionDenied {
+    @Scenario("Customer trying to register an Event")
+    @Given("Some Post, a Customer User, and the Current Day")
+    @When("Trying to register a new future Event as a Customer")
+    @Then("Should not register and throw PermissionDenied")
+    @Test
+    void shouldNotAllow() {
+        assertThrows(PermissionDenied.class, () -> {
             var poster = somePoster();
             var user = someCustomer();
             var currentDay = LocalDate.of(2024, 10, 1);
             new EventRegistering().into(repository).poster(poster).by(user)
-                    .on(currentDay);
-        }
-
-        @Then("Should not register and throw PermissionDenied")
-        @Test
-        void shouldNotAllow() {
-            assertThrows(PermissionDenied.class, () -> tryRegister());
-            assertFalse(
-                    repository.has("From Zero", LocalDate.of(2024, 10, 15)));
-        }
+                    .on(currentDay).register();
+        });
+        assertFalse(repository.has("From Zero", LocalDate.of(2024, 10, 15)));
     }
 
-    @Nested
     @Scenario("Admin trying to re-register an Event")
-    @Given("Some Post, an Admin User and the Current Day")
-    class UnsuccessfulForAlreadyRegistered {
-
-        @When("Trying to register an already registered Event")
-        void registerTwice() throws PermissionDenied, EventAlreadyRegistered,
-                CantRegisterPastEvent {
+    @Given("Some Post, an Admin User, and the Current Day")
+    @When("Trying to register an already registered Event")
+    @Then("Should not re-register and throw EventAlreadyRegistered")
+    @Test
+    void shouldNotAllowReRegister() {
+        assertThrows(EventAlreadyRegistered.class, () -> {
             var poster = somePoster();
             var user = someAdmin();
             var currentDay = LocalDate.of(2024, 10, 1);
@@ -131,58 +130,47 @@ public class EventRegisteringFeature {
                     .by(user).on(currentDay);
             context.register();
             context.register();
-        }
-
-        @Test
-        @Then("Should not re-register and throw EventAlreadyRegistered")
-        void shouldNotAllow() {
-            assertThrows(EventAlreadyRegistered.class, () -> registerTwice());
-            assertTrue(repository.has("From Zero", LocalDate.of(2024, 10, 15)));
-        }
+        });
+        assertTrue(repository.has("From Zero", LocalDate.of(2024, 10, 15)));
     }
 
-    @Nested
     @Scenario("An Admin trying to register a past Event")
-    @Given("Some Post, an Admin User and the Current Day")
-    class UnsuccessfulForPastEvent {
-
-        @When("Trying to register a past Event")
-        void tryRegister(LocalDate today) throws PermissionDenied,
-                EventAlreadyRegistered, CantRegisterPastEvent {
+    @Given("Some Post, an Admin User, and the Current Day")
+    @When("Trying to register a past Event")
+    @Then("Should not register on the same day")
+    @Test
+    void shouldNotRegisterForSameDate() {
+        // Pre-Condition
+        var today = LocalDate.of(2024, 10, 15);
+        assumeTrue(somePoster().date().isEqual(today));
+        // Do
+        assertThrows(CantRegisterPastEvent.class, () -> {
             var poster = somePoster();
             var user = someAdmin();
             new EventRegistering().into(repository).poster(poster).by(user)
                     .on(today).register();
-        }
+        });
+        // Post-Condition
+        assertFalse(repository.has("From Zero", LocalDate.of(2024, 10, 15)));
+    }
 
-        @Then("Should not register on same day")
-        @Test
-        void shouldNotRegisterForSameDate() {
-            // Pre-Condition
-            var today = LocalDate.of(2024, 10, 15);
-            assumeTrue(somePoster().date().isEqual(today));
-
-            // Do
-            assertThrows(CantRegisterPastEvent.class, () -> tryRegister(today));
-
-            // Post-Condition
-            assertFalse(
-                    repository.has("From Zero", LocalDate.of(2024, 10, 15)));
-        }
-
-        @Then("Should not register for past days")
-        @Test
-        void shouldNotRegisterForPastDay() {
-            // Pre-Condition
-            var today = LocalDate.of(2024, 10, 16);
-            assumeTrue(somePoster().date().isBefore(today));
-
-            // Do
-            assertThrows(CantRegisterPastEvent.class, () -> tryRegister(today));
-
-            // Post-Condition
-            assertFalse(
-                    repository.has("From Zero", LocalDate.of(2024, 10, 15)));
-        }
+    @Scenario("An Admin trying to register a past Event")
+    @Given("Some Post, an Admin User, and the Current Day")
+    @When("Trying to register a past Event")
+    @Then("Should not register for past days")
+    @Test
+    void shouldNotRegisterForPastDay() {
+        // Pre-Condition
+        var today = LocalDate.of(2024, 10, 16);
+        assumeTrue(somePoster().date().isBefore(today));
+        // Do
+        assertThrows(CantRegisterPastEvent.class, () -> {
+            var poster = somePoster();
+            var user = someAdmin();
+            new EventRegistering().into(repository).poster(poster).by(user)
+                    .on(today).register();
+        });
+        // Post-Condition
+        assertFalse(repository.has("From Zero", LocalDate.of(2024, 10, 15)));
     }
 }
