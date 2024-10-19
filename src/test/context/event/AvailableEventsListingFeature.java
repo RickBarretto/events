d015045da55232433835928a4ffd12c89af77c77
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 12:22:57 -0300

    refactor test

--- src/test/context/event/AvailableEventsListingFeature.java
+++ src/test/context/event/AvailableEventsListingFeature.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,79 +21,82 @@ import test.resources.bdd.Given;
 import test.resources.bdd.Then;
 import test.resources.bdd.When;
 
+class AvailableEvents {
+    private static final List<Event> events = List.of(
+            new Event(
+                    new Poster("Available", "...", LocalDate.of(2024, 10, 15))),
+            new Event(
+                    new Poster("Available", "...", LocalDate.of(2024, 10, 16))),
+            new Event(
+                    new Poster("Available", "...", LocalDate.of(2024, 11, 15))),
+            new Event(
+                    new Poster("Available", "...", LocalDate.of(2024, 10, 15))),
+            new Event(
+                    new Poster("Available", "...", LocalDate.of(2030, 1, 15))));
+
+    public static List<Event> all() { return events; }
+}
+
+class UnavailableEvents {
+    private static final List<Event> events = List.of(
+            new Event(new Poster("Unavailable", "...",
+                    LocalDate.of(2023, 10, 15))),
+            new Event(new Poster("Unavailable", "...",
+                    LocalDate.of(2024, 10, 14))),
+            new Event(new Poster("Unavailable", "...",
+                    LocalDate.of(2024, 9, 16))),
+            new Event(new Poster("Unavailable", "...",
+                    LocalDate.of(2022, 10, 15))));
+
+    public static List<Event> all() { return events; }
+}
+
 @Feature("Listing available events")
-@Given("A group of available and not available Events")
 public class AvailableEventsListingFeature {
     LocalDate today;
     Events allEvents;
     List<Event> listedEvents;
 
-    List<Event> available;
-    List<Event> unavailable;
-
-    List<Event> available() {
-        return List.of(
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 10, 15))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 10, 16))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 11, 15))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 10, 15))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2030, 1, 15))));
-    }
-
-    List<Event> unavailable() {
-        return List.of(
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2023, 10, 15))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 10, 14))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 9, 16))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2022, 10, 15))));
-    }
-
     @BeforeEach
     void init() {
         this.today = LocalDate.of(2024, 10, 15);
-        this.available = available();
-        this.unavailable = unavailable();
-        this.allEvents = new EventsInMemory();
 
-        for (Event event : available) {
-            this.allEvents.register(event);
-        }
-
-        for (Event event : unavailable) {
-            this.allEvents.register(event);
-        }
+        // @formatter:off
+        this.allEvents = new EventsInMemory(
+            Stream.concat(
+                AvailableEvents.all().stream(), UnavailableEvents.all().stream()
+            ).collect(Collectors.toList()
+        ));
+        // @formatter:on
     }
 
-    @When("When listing available events")
-    void whenListing() {
-        this.listedEvents = new AvailableEventsListing().from(allEvents)
-                .beingToday(today).availables();
+    void listEvents() {
+        // @formatter:off
+        this.listedEvents = new AvailableEventsListing()
+            .from(allEvents)
+            .beingToday(today)
+            .availables();
+        // @formatter:on
     }
 
     @Test
+    @Given("Available and Unavailable Events")
+    @When("When listing Available Events")
     @Then("Listed Events should include all available ones")
     void shouldIncludeAvailables() {
-        whenListing();
-        assertTrue(
-            listedEvents.stream().allMatch((listed) -> available.contains(listed))
-        );
+        listEvents();
+        assertTrue(listedEvents.stream()
+                .allMatch((listed) -> AvailableEvents.all().contains(listed)));
     }
 
     @Test
+    @Given("Available and Unavailable Events")
+    @When("When listing Available Events")
     @Then("Listed Events should ignore all unavailable ones")
     void shouldIgnorePastEvents() {
-        whenListing();
-        assertFalse(listedEvents.stream()
-                .anyMatch((listed) -> unavailable.contains(listed)));
+        listEvents();
+        assertFalse(listedEvents.stream().anyMatch(
+                (listed) -> UnavailableEvents.all().contains(listed)));
     }
 
 }
