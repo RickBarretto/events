ae8a768ead6df599241f76c073961414cfdae3a4
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 18:02:11 -0300

    make it clearer with BDD

--- src/test/context/user/UserEditingFeature.java
+++ src/test/context/user/UserEditingFeature.java
@@ -1,18 +1,15 @@
 package test.context.user;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeFalse;
-import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
 import main.domain.contexts.user.UserEditing;
 import main.domain.exceptions.EmailAlreadyExists;
 import main.domain.exceptions.InexistentUser;
@@ -21,173 +18,135 @@ import main.domain.models.users.Login;
 import main.domain.models.users.Person;
 import main.domain.models.users.User;
 import main.roles.repositories.Users;
-
-// Test supporters
 import test.resources.bdd.*;
 import test.resources.entities.ConcreteUsers;
 
-// @formatter:off
 @Feature("Editing some existing User")
 public class UserEditingFeature {
 
-    @Nested
-    @Scennario("Sucessfully editing some existing User")
-    @Given("Some existing user in some Repository")
-    class Sucessfully {
-        Users repository;
-        Login expectedLogin;
-        Person expectedPerson;
+    private Users repository;
+    private Login expectedLogin;
+    private Person expectedPerson;
 
-        @BeforeEach
-        void registerTargetUser() {
-            repository = ConcreteUsers.empty();
-            repository.register(ConcreteUsers.JohnDoe());
-        }
-
-        @BeforeEach
-        void submitForms() {
-            expectedLogin = new Login("jane.doe@example.com", "789123");
-            expectedPerson = new Person("Jane Doe", "111.111.111-11");
-        }
-
-        @When("Editing the Login and Person of an User")
-        void editLogin() throws InexistentUser, EmailAlreadyExists {
-            new UserEditing(repository)
-                .of(ConcreteUsers.JohnDoe())
-                .changing()
-                    .email("jane.doe@example.com")
-                    .password("789123")
-                    .name("Jane Doe")
-                    .cpf("111.111.111-11")
-                .edit();
-        }
-
-        @Test
-        @Then("Should update the Login if user exists")
-        void shouldUpdateLogin() {
-            var oldEmail = "john.doe@example.com";
-            var newEmail = "jane.doe@example.com";
-
-            // Pre-condition
-            assumeTrue("Old email is registered", repository.has(oldEmail));
-
-            // Execution
-            assertDoesNotThrow(() -> editLogin());
-
-            // Post-condition
-            assertAll("Email was updated",
-                    () -> assertFalse(repository.has(oldEmail)),
-                    () -> assertTrue(repository.has(newEmail))
-            );
-
-            var updatedUser = repository.ownerOf(newEmail, "789123").get();
-            assertEquals("Updated Login", expectedLogin, updatedUser.login());
-            assertEquals("Updated Person", expectedPerson, updatedUser.person());
-        }
-
-        @Test
-        @And("Should not change the ID")
-        void shouldNotChangeTheID() {
-            // Execution
-            assertDoesNotThrow(() -> editLogin());
-
-            // Post-condition
-            var updatedUser = repository.ownerOf("jane.doe@example.com", "789123").get();
-            assertEquals(ConcreteUsers.JohnDoe().id(), updatedUser.id());
-        }
+    @BeforeEach
+    void registerTargetUser() {
+        repository = ConcreteUsers.empty();
+        repository.register(ConcreteUsers.JohnDoe());
     }
 
-    @Nested
-    @Scennario("Trying editing some existing User to an existing email")
-    @Given("Some existing user in some Repository")
-    class NotAvailableEmail {
-        Users repository;
+    @BeforeEach
+    void submitForms() {
+        expectedLogin = new Login("jane.doe@example.com", "789123");
+        expectedPerson = new Person("Jane Doe", "111.111.111-11");
+    }
 
-        @BeforeEach
-        void registerTargetUserAndOther() {
-            repository = ConcreteUsers.empty();
-            repository.register(ConcreteUsers.JohnDoe());
-            repository.register(new User(
-                new Login("jane.doe@example.com", "789123"),
-                new Person("Jane Doe", "111.111.111-11")
-            ));
-        }
+    @Scenario("Successfully editing an existing User")
+    @Given("An existing user in the repository")
+    @When("Editing the Login and Person details of the User")
+    @Then("Should update the Login if the user exists")
+    @Test
+    void shouldUpdateLogin() {
+        var oldEmail = "john.doe@example.com";
+        var newEmail = "jane.doe@example.com";
+        // Given
+        assumeTrue(repository.has(oldEmail), "Old email is registered");
 
-        @When("Editing the Login and Person of an User")
-        void editToExistingEmail() throws InexistentUser, EmailAlreadyExists {
-            new UserEditing(repository)
-                .of(ConcreteUsers.JohnDoe())
-                .changing()
-                    .email("jane.doe@example.com");
-        }
+        // When
+        assertDoesNotThrow(() -> {
+            new UserEditing(repository).of(ConcreteUsers.JohnDoe()).changing()
+                    .email("jane.doe@example.com").password("789123")
+                    .name("Jane Doe").cpf("111.111.111-11").edit();
+        });
 
-        @Test
-        @Then("Should thows EmailAlreadyExists and not update")
-        void shouldUpdateLogin() {
-            // Pre-condition
-            assumeTrue("User is registered", repository.has("john.doe@example.com"));
-            
-            // Execution
-            assertThrows(EmailAlreadyExists.class, () -> editToExistingEmail());
-            
-            // Post-condition
-            assertAll("User was not updated", 
+        // Then
+        assertAll("Email was updated",
+                () -> assertFalse(repository.has(oldEmail)),
+                () -> assertTrue(repository.has(newEmail)));
+
+        var updatedUser = repository.ownerOf(newEmail, "789123").get();
+        assertEquals(expectedLogin, updatedUser.login());
+        assertEquals(expectedPerson, updatedUser.person());
+    }
+
+    @Scenario("Successfully editing an existing User")
+    @Given("An existing user in the repository")
+    @When("Editing the Login and Person details of the User")
+    @Then("Should not change the ID")
+    @Test
+    void shouldNotChangeTheID() {
+        // When
+        assertDoesNotThrow(() -> {
+            new UserEditing(repository).of(ConcreteUsers.JohnDoe()).changing()
+                    .email("jane.doe@example.com").password("789123")
+                    .name("Jane Doe").cpf("111.111.111-11").edit();
+        });
+
+        // Then
+        var updatedUser = repository.ownerOf("jane.doe@example.com", "789123")
+                .get();
+        assertEquals(ConcreteUsers.JohnDoe().id(), updatedUser.id());
+    }
+
+    @Scenario("Trying to edit an existing User to an existing email")
+    @Given("An existing user in the repository")
+    @When("Editing the Login and Person details of the User to an existing email")
+    @Then("Should throw EmailAlreadyExists and not update")
+    @Test
+    void shouldNotUpdateToExistingEmail() {
+        repository
+                .register(new User(new Login("jane.doe@example.com", "789123"),
+                        new Person("Jane Doe", "111.111.111-11")));
+        // Given
+        assumeTrue(
+                repository.has("john.doe@example.com"), "User is registered");
+        
+        // When
+        assertThrows(EmailAlreadyExists.class, () -> {
+            new UserEditing(repository).of(ConcreteUsers.JohnDoe()).changing()
+                    .email("jane.doe@example.com").edit();
+        });
+        
+        // Then
+        assertAll("User was not updated",
                 () -> assertTrue(repository.has("john.doe@example.com")),
-                () -> assertTrue(repository.has("jane.doe@example.com"))
-            );
-        }
+                () -> assertTrue(repository.has("jane.doe@example.com")));
     }
 
-    @Nested
-    @Scennario("Trying to edit an inexistent user")
-    @Given("Some inexistent User")
-    class UserDoesNotExist {
-        Users repository;
-
-        @BeforeEach
-        void registerTargetUserAndOther() {
-            repository = ConcreteUsers.empty();
-        }
-
-        User inexistenUser() {
-            return ConcreteUsers.JohnDoe();
-        }
-
-        @When("Editing the Login and Person of an User")
-        void editInexistentUser() throws InexistentUser {
-            new UserEditing(repository)
-                .of(inexistenUser())
-                .changing();
-        }
-
-        @Test
-        @Then("Should throw InexistentUser, and not register it.")
-        void shouldThrow() {
-            // Pre-condition
-            assumeFalse(repository.has("john.doe@example.com"));
-
-            // Execution
-            assertThrows(InexistentUser.class, () -> this.editInexistentUser());
-            
-            // Post-condition
-            assertFalse(repository.has("john.doe@example.com"));
-        }
+    @Scenario("Trying to edit a nonexistent user")
+    @Given("A nonexistent User")
+    @When("Editing the Login and Person details of the nonexistent User")
+    @Then("Should throw InexistentUser and not register it.")
+    @Test
+    void shouldThrowInexistentUser() {
+        // Given
+        assumeFalse(repository.has("john.doe@example.com"));
+        
+        // When
+        assertThrows(InexistentUser.class, () -> {
+            new UserEditing(repository).of(ConcreteUsers.JohnDoe()).changing()
+                    .edit();
+        });
+        
+        // Then
+        assertFalse(repository.has("john.doe@example.com"));
     }
 
-    @Nested
-    @Scennario("Replace password requires old password")
-    @Given("")
-    class ReplacePasswordNeedsPermission {
-        Users repository;
-
-        @BeforeEach
-        void registerTargetUserAndOther() {
-            repository = ConcreteUsers.empty();
-        }
-
-        @When("Editing the Login and Person of an User")
-        void editToExistingEmail() throws InexistentUser, PermissionDenied, EmailAlreadyExists {
-        }
-    }
-
+    // @Scenario("Replace password requires old password")
+    // @Given("An existing user in the repository")
+    // @When("Editing the Login and Person details of the User without providing the old password")
+    // @Then("Should throw PermissionDenied")
+    // @Test
+    // void shouldThrowPermissionDenied() {
+    //     // Pre-condition
+    //     assumeTrue(repository.has("john.doe@example.com"));
+    //     // Execution
+    //     assertThrows(PermissionDenied.class, () -> {
+    //         new UserEditing(repository).of(ConcreteUsers.JohnDoe()).changing()
+    //                 .password("newpassword") // Old password not provided
+    //                 .edit();
+    //     });
+    //     // Post-condition
+    //     var user = repository.ownerOf("john.doe@example.com", "123456").get();
+    //     assertEquals("123456", user.login().password());
+    // }
 }
