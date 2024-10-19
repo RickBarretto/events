8eb8640b7ece633fc06f192253b05d0de388117d
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 14:51:32 -0300

    deprecate unused constructor

--- src/main/domain/models/evaluations/EvaluationId.java
+++ src/main/domain/models/evaluations/EvaluationId.java
@@ -1,11 +1,7 @@
 package main.domain.models.evaluations;
 
-import java.util.UUID;
-
 import main.roles.EntityId;
 
 public class EvaluationId extends EntityId {
-    public EvaluationId(UUID id) { super(id); }
-
     public EvaluationId() { super(); }
 }
