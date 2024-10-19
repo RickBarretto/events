6ec5eac563ba567d45de4d61cc79491f2c14a6fb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 17:01:42 -0300

    add javadoc

--- src/main/roles/Entity.java
+++ src/main/roles/Entity.java
@@ -1,5 +1,17 @@
 package main.roles;
 
+/**
+ * Represents a generic entity within the domain. This interface defines a
+ * contract for entities to provide their unique identifier.
+ *
+ * @param <Id> the type of the entity's identifier
+ */
 public interface Entity<Id extends EntityId> {
+
+    /**
+     * Returns the unique identifier of the entity.
+     *
+     * @return the entity's unique identifier
+     */
     public Id id();
 }
