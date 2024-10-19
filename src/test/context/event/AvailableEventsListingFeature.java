50204604f5e5672d0bc7b9e36528cb7cf449134b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 18:51:46 -0300

    make it clearer with BDD

--- src/test/context/event/AvailableEventsListingFeature.java
+++ src/test/context/event/AvailableEventsListingFeature.java
@@ -1,7 +1,7 @@
 package test.context.event;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,11 +13,11 @@ import main.infra.virtual.EventsInMemory;
 import main.roles.repositories.Events;
 import test.resources.bdd.Feature;
 import test.resources.bdd.Given;
+import test.resources.bdd.Scenario;
 import test.resources.bdd.Then;
 import test.resources.bdd.When;
 
 @Feature("Listing available events")
-@Given("A group of available and not available Events")
 public class AvailableEventsListingFeature {
 
     private LocalDate today;
@@ -36,49 +36,44 @@ public class AvailableEventsListingFeature {
         unavailable.forEach(allEvents::register);
     }
 
+    // @formatter:off
     List<Event> availableEvents() {
         return List.of(
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 10, 15))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 10, 16))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 11, 15))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2024, 10, 15))),
-                new Event(new Poster("Available", "...",
-                        LocalDate.of(2030, 1, 15))));
+            new Event(new Poster("Available", "...", LocalDate.of(2024, 10, 15))),
+            new Event(new Poster("Available", "...", LocalDate.of(2024, 10, 16))),
+            new Event(new Poster("Available", "...", LocalDate.of(2024, 11, 15))),
+            new Event(new Poster("Available", "...", LocalDate.of(2024, 10, 15))),
+            new Event(new Poster("Available", "...", LocalDate.of(2030, 1, 15))));
     }
-
+                    
     List<Event> unavailableEvents() {
         return List.of(
-                new Event(new Poster("Unavailable", "...",
-                        LocalDate.of(2023, 10, 15))),
-                new Event(new Poster("Unavailable", "...",
-                        LocalDate.of(2024, 10, 14))),
-                new Event(new Poster("Unavailable", "...",
-                        LocalDate.of(2024, 9, 16))),
-                new Event(new Poster("Unavailable", "...",
-                        LocalDate.of(2022, 10, 15))));
+            new Event(new Poster("Unavailable", "...", LocalDate.of(2023, 10, 15))),
+            new Event(new Poster("Unavailable", "...", LocalDate.of(2024, 10, 14))),
+            new Event(new Poster("Unavailable", "...", LocalDate.of(2024, 9, 16))),
+            new Event(new Poster("Unavailable", "...", LocalDate.of(2022, 10, 15))));
     }
+    // @formatter:on
 
-    @When("When listing available events")
-    void whenListing() {
+    @Scenario("Listing available events")
+    @Given("A group of available and not available Events")
+    @When("Listing available events")
+    @Then("Listed Events should include all available ones")
+    @Test
+    void shouldIncludeAvailables() {
         listedEvents = new AvailableEventsListing().from(allEvents)
                 .beingToday(today).availables();
-    }
-
-    @Test
-    @Then("Listed Events should include all available ones")
-    void shouldIncludeAvailables() {
-        whenListing();
         assertTrue(listedEvents.stream().allMatch(available::contains));
     }
 
-    @Test
+    @Scenario("Listing available events")
+    @Given("A group of available and not available Events")
+    @When("Listing available events")
     @Then("Listed Events should ignore all unavailable ones")
+    @Test
     void shouldIgnorePastEvents() {
-        whenListing();
+        listedEvents = new AvailableEventsListing().from(allEvents)
+                .beingToday(today).availables();
         assertFalse(listedEvents.stream().anyMatch(unavailable::contains));
     }
 }
