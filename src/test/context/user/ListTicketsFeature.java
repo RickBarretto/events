dd77ac5a665a1e6590c987301454f6c5dc56c54b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 17:41:29 -0300

    make it clearer with BDD

--- src/test/context/user/ListTicketsFeature.java
+++ src/test/context/user/ListTicketsFeature.java
@@ -1,49 +1,36 @@
 package test.context.user;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assume.assumeTrue;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import main.domain.contexts.user.ListTickets;
 import main.domain.models.events.EventId;
 import main.domain.models.events.Ticket;
 import main.domain.models.users.User;
-
-// Test supporters
-import test.resources.bdd.*;
 import test.resources.entities.ConcreteUsers;
+import test.resources.bdd.*;
 
-// @formatter:off
+@Feature("Listing User Tickets")
 public class ListTicketsFeature {
-    List<Ticket> tickets = List.of(
-        new Ticket(new EventId()),
-        new Ticket(new EventId())
-    );
 
-    User customer;
+    private List<Ticket> tickets;
+    private User customer;
 
     @BeforeEach
     void initCustomer() {
+        tickets = List.of(new Ticket(new EventId()), new Ticket(new EventId()));
         customer = ConcreteUsers.JohnDoe().withTickets(tickets);
     }
 
-    @Assume("A customer with two Tickets")
-    void userShouldHaveTwoTickets() {
-        assumeTrue(2 == customer.tickets().size());
-    }
-
+    @Given("A customer with two tickets")
+    @Assume("The customer has two tickets")
     @When("Listing their tickets")
-    List<Ticket> listTickets() {
-        return ListTickets.ownedBy(customer).list();
-    }
-    
-    @Test
     @Then("Should return the same tickets")
+    @Test
     void shouldReturnTheSameTickets() {
-        assertEquals(tickets, listTickets());
-    }   
+        assumeTrue(2 == customer.tickets().size());
+        assertEquals(tickets, ListTickets.ownedBy(customer).list());
+    }
 }
