ebc5aadb4967112b0b8ddbddb12a6df9ecabaf0f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 13:24:22 -0300

    implement factory for tests

---
+++ src/test/resources/entities/ConcreteUsers.java
@@ -0,0 +1,33 @@
+package test.resources.entities;
+
+import java.util.List;
+
+import main.domain.models.users.Login;
+import main.domain.models.users.Person;
+import main.domain.models.users.User;
+import main.infra.virtual.UsersInMemory;
+
+public class ConcreteUsers {
+
+    private static User johnDoe = new User(
+            new Login("john.doe@example.com", "123456"),
+            new Person("John Doe", "000.000.000-00"));
+    private static User janeDoe = new User(
+            new Login("jane.doe@example.com", "789123"),
+            new Person("Jane Doe", "111.111.111-11"));
+
+    static public User JohnDoe() { return johnDoe; }
+
+    static public User JaneDoe() { return janeDoe; }
+
+    static public UsersInMemory empty() { return new UsersInMemory(); }
+
+    static public UsersInMemory withJohn() {
+        return new UsersInMemory(List.of(JohnDoe()));
+    }
+
+    static public UsersInMemory withJohnAndJane() {
+        return new UsersInMemory(List.of(JohnDoe(), JaneDoe()));
+    }
+
+}
