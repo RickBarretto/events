3b38ff1269c23abcc98acad1192cc73832dca5d9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-19 17:23:09 -0300

    and @And repeatable

--- src/test/resources/bdd/And.java
+++ src/test/resources/bdd/And.java
@@ -1,7 +1,12 @@
 package test.resources.bdd;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
+@interface AndContext { And[] value(); }
+
+@Target(ElementType.METHOD)
+@Repeatable(AndContext.class)
 public @interface And { String value(); }
