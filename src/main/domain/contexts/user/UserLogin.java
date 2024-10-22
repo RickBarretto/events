61b4a352ba006b6a4f89ca21462a6ce93a02d691
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-21 21:33:05 -0300

    add javadoc

--- src/main/domain/contexts/user/UserLogin.java
+++ src/main/domain/contexts/user/UserLogin.java
@@ -5,22 +5,45 @@ import main.infra.Session;
 import main.roles.Context;
 import main.roles.repositories.Users;
 
+/**
+ * Context for managing user login operations.
+ */
 public class UserLogin implements Context {
     private Session session;
     private Users users;
 
+    /**
+     * Constructs a UserLogin context with the specified user repository.
+     *
+     * @param users the repository of users
+     */
     public UserLogin(Users users) { this.users = users; }
 
+    /**
+     * Associates the current login context with a session.
+     *
+     * @param session the session to be associated
+     * @return the current UserLogin context
+     */
     public UserLogin withSession(Session session) {
         this.session = session;
         return this;
     }
 
+    /**
+     * Logs in the user with the specified email and password.
+     *
+     * @param email    the email of the user
+     * @param password the password of the user
+     * @throws PermissionDenied if the login credentials are incorrect
+     */
     public void logAs(String email, String password) throws PermissionDenied {
         session.logInAs(users.ownerOf(email, password).orElseThrow(
                 () -> new PermissionDenied("Wrong login credentials")));
     }
 
+    /**
+     * Logs out the current session.
+     */
     public void logOut() { session.logOut(); }
-
 }
