8d9f22188ec1b8ea91a7a73ea4195972e00cbc20
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-21 21:29:45 -0300

    add javadoc

--- src/main/domain/contexts/user/ListTickets.java
+++ src/main/domain/contexts/user/ListTickets.java
@@ -1,23 +1,39 @@
 package main.domain.contexts.user;
 
 import java.util.List;
-
 import main.domain.models.events.Ticket;
 import main.domain.models.users.User;
 import main.roles.Context;
 
+/**
+ * Context for listing tickets owned by a user.
+ */
 public class ListTickets implements Context {
     private User customer;
 
-    private ListTickets(User customer) {
-        this.customer = customer;
-    }
+    /**
+     * Private constructor for creating a ListTickets context with the specified
+     * user.
+     *
+     * @param customer the user whose tickets are to be listed
+     */
+    private ListTickets(User customer) { this.customer = customer; }
 
+    /**
+     * Static factory method for creating a ListTickets context for a specific
+     * user.
+     *
+     * @param customer the user whose tickets are to be listed
+     * @return a new ListTickets context for the specified user
+     */
     public static ListTickets ownedBy(User customer) {
         return new ListTickets(customer);
     }
 
-    public List<Ticket> list() {
-        return customer.tickets();
-    }
+    /**
+     * Retrieves the list of tickets owned by the user.
+     *
+     * @return a list of tickets owned by the user
+     */
+    public List<Ticket> list() { return customer.tickets(); }
 }
