60876b3eb6eeeead1555710bbf240047436b1ff9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-22 19:35:39 -0300

    add javadoc

--- src/main/roles/repositories/Users.java
+++ src/main/roles/repositories/Users.java
@@ -2,20 +2,58 @@ package main.roles.repositories;
 
 import java.util.List;
 import java.util.Optional;
-
 import main.domain.models.users.User;
 import main.domain.models.users.UserId;
 
+/**
+ * Interface for managing User entities.
+ */
 public interface Users {
+
+    /**
+     * Registers a new user.
+     *
+     * @param user the user to be registered
+     */
     void register(User user);
 
+    /**
+     * Updates an existing user.
+     *
+     * @param target  the user to be updated
+     * @param newUser the new user information
+     */
     void update(User target, User newUser);
 
+    /**
+     * Retrieves an user by their ID.
+     *
+     * @param id the ID of the user
+     * @return an Optional containing the user if found, or empty if not found
+     */
     Optional<User> byId(UserId id);
 
+    /**
+     * Retrieves an user by their email and password.
+     *
+     * @param email    the email of the user
+     * @param password the password of the user
+     * @return an Optional containing the user if found, or empty if not found
+     */
     Optional<User> ownerOf(String email, String password);
 
+    /**
+     * Checks if an user exists by their email.
+     *
+     * @param email the email of the user
+     * @return true if the user exists, false otherwise
+     */
     boolean has(String email);
 
+    /**
+     * Lists all users.
+     *
+     * @return a list of all users
+     */
     List<User> list();
 }
