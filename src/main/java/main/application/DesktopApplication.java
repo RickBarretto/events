f875b416afb5c0bfcaf9fb8cbbdcaeb1f9b88e68
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-09 21:43:41 -0300

    set-up maven
    yes, I know 103 files to be commited at once...

---
+++ src/main/java/main/application/DesktopApplication.java
@@ -0,0 +1,21 @@
+package main.application;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class DesktopApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) { launch(); }
+}
